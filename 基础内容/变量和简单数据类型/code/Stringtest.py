98b010075386703125ed89c4fce23515311db777
AcherYAN <396507593>
2019-10-23 17:58:31 +0800

    20191023

---
+++ 基础内容/变量和简单数据类型/code/Stringtest.py
@@ -0,0 +1,14 @@
+name = 'jack black'
+print(name.title())
+
+first_name = 'Jack'
+last_name = 'Black'
+full_name = first_name + " " + last_name
+print('Hello,' + full_name + '!')
+
+print("\tPython")
+print('\nPython\nC++\nJava\nHtml')
+
+str1 = '  jack black  '
+print(str1.rstrip())
+print(str1.lstrip())
